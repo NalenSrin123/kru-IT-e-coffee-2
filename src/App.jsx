61e6ec3573e79a52e426b6d1c30c6f3260c2b194
Sivnean <mar456992@gmail.com>
2026-03-01 17:53:03 +0700

    Implement CoffeeLogin component and update main entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+// import React from 'react'
 
-const App = () => {
-  return (
-    <div className='text-red-500'>App</div>
-  )
-}
+// const App = () => {
+//   return (
+//     <div className='text-red-500'>App</div>
+//   )
+// }
 
-export default App
+// export default App
