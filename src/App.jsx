ab7df7f6da9b3f8c959f5fb380255300528ea0c4
KingWep <thaneverlose168@gmail.com>
2026-03-02 19:07:36 +0700

    Build Form_Comfirm_OTP

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Form_Confirm_OTP from './services/auth/Form_Confirm_OTP'
 
 const App = () => {
   return (
-    <div className='text-red-500 text-2xl'>App</div>
+    <div className='text-red-500 text-2xl'>
+      <Form_Confirm_OTP />
+    </div>
   )
 }
 
