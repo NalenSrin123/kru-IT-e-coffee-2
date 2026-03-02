08e05b2adc3d52e5cacf3294f1a19e5e756f92c8
Devith <yanxiao007v@gmail.com>
2026-03-02 21:47:34 +0700

    Design_checkout_page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Design_checkout_page from './components/public/checkout/Design_checkout_page'
 
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+    <div className=''>
+
+      <Design_checkout_page/>
+    </div>
+    
   )
 }
 
