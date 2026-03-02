2b96d5ac1062e8ff8e0f1c796d3f87cf9cfc6d30
NalenSrin123 <srinnalen@gmail.com>
2026-03-02 22:23:41 +0700

    Fixed issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Form_Confirm_OTP from './services/auth/Form_Confirm_OTP'
 
 import Footer from "./components/public/Footer/Footer";
 import Reset_password from './services/auth/Reset_password'
-import React from "react";
+// import React from "react";
 import Registerform from "./services/auth/Registerform";
 import ResetPassword from './services/auth/ResetPassword';
 const App = () => {
@@ -12,11 +12,8 @@ const App = () => {
     <div className=''>
 
       <Design_checkout_page/>
-    </div>
-    
-    <div className='text-red-500 text-2xl'>
       <Form_Confirm_OTP />
-    <div>
+
       <Registerform />
 <Reset_password/>
       <ResetPassword />
