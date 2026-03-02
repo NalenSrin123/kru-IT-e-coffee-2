9a292c55b10c99b400cb374a774ba60ae298058e
KingWep <thaneverlose168@gmail.com>
2026-03-02 23:05:16 +0700

    update OTP form

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import Form_Confirm_OTP from './services/auth/Form_Confirm_OTP'
-
-import Footer from "./components/public/Footer/Footer";
-import Reset_password from './services/auth/Reset_password'
 import React from "react";
+import Design_checkout_page from "./components/public/checkout/Design_checkout_page";
+import Form_Confirm_OTP from "./services/auth/Form_Confirm_OTP";
+import Footer from "./components/public/Footer/Footer";
+import Reset_password from "./services/auth/Reset_password";
 import Registerform from "./services/auth/Registerform";
-import ResetPassword from './services/auth/ResetPassword';
+import ResetPassword from "./services/auth/ResetPassword";
+
 const App = () => {
   return (
-    <div className='text-red-500 text-2xl'>
-      <Form_Confirm_OTP />
     <div>
-      <Registerform />
-<Reset_password/>
+      {/* <Design_checkout_page /> */}
+      <Form_Confirm_OTP />
+      {/* <Registerform />
+      <Reset_password />
       <ResetPassword />
-    <Footer />
+      <Footer /> */}
     </div>
-  )
-}
+  );
+};
+
 export default App;
