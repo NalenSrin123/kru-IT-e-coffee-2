6234af41dbf3307cb30a15ac856ccd74a7e86aea
meyching88 <loemmeyching27@gmail.com>
2026-03-03 10:53:02 +0700

    Design Uesr Order Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import UserOrderPage from './components/public/user_profile/uers_order_page/order_page.jsx'
 
 const App = () => {
   return (
+    <>
+    <UserOrderPage/>
     <div className='text-red-500 text-2xl'>App</div>
+    </>
+    
   )
 }
 
