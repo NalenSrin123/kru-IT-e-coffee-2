24238ee16d8bba996076d576f24114e62ffa2480
Jiierr1 <menchhorvy3@gmail.com>
2026-03-04 23:17:05 +0700

    Design_user_address_page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
+import React from "react";
+import Desgin_use_addres_page from "./components/public/user_profile/Desgin_use_addres_page";
 const App = () => {
-  return (
-    <div className='text-red-500 text-2xl'>App</div>
-  )
-}
+  return <Desgin_use_addres_page />;
+};
 
-export default App
+export default App;
