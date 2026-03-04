0a65e270c9ed627781388b92cb3b9a78e9f09f86
samphorss <samphors.hong.student@gmail.com>
2026-03-04 18:18:42 +0700

    Design Service page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react'
 
 const App = () => {
@@ -5,5 +6,28 @@ const App = () => {
     <div className='text-red-500 text-2xl'>App</div>
   )
 }
+=======
+import React from "react";
+import Design_checkout_page from "./components/public/checkout/Design_checkout_page";
+import Form_Confirm_OTP from "./services/auth/Form_Confirm_OTP";
+import Footer from "./components/public/Footer/Footer";
+import Reset_password from "./services/auth/Reset_password";
+import Registerform from "./services/auth/Registerform";
+import ResetPassword from "./services/auth/ResetPassword";
+import Design_Service_Page from "./components/public/service/Design_Service_Page";
+const App = () => {
+  return (
+    <div>
+      {/* <Design_checkout_page /> */}
+      {/* <Form_Confirm_OTP /> */}
+      {/* <Registerform />
+      <Reset_password />
+      <ResetPassword />
+      <Footer /> */}
+      <Design_Service_Page/>
+    </div>
+  );
+};
+>>>>>>> Stashed changes
 
 export default App
