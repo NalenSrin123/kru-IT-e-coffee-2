09db48ee9a165abaa7ae17815f55d1f9d688f2f8
unknown <meanlove088@gmail.com>
2026-03-14 15:19:01 +0700

    resolve merge conflict App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-
-import Navbar from "./components/public/home/Navbar";
-import Hero_section from "./components/public/home/Hero_section";
-import Design_checkout_page from "./components/public/checkout/Design_checkout_page";
-
-import Footer from "./components/public/Footer/Footer";
-
-import Form_Confirm_OTP from "./services/auth/Form_Confirm_OTP";
-import Reset_password from "./services/auth/Reset_password";
-import Registerform from "./services/auth/Registerform";
-=======
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Layouts
@@ -26,7 +14,6 @@ import About from "./components/public/about/About";
 // Auth Pages
 import Login from "./context/auth/form_login";
 import Register from "./services/auth/Registerform";
->>>>>>> a8b59b019b668694f02e65b3ca49cc106f249cd4
 import ResetPassword from "./services/auth/ResetPassword";
 import SendResetPassword from "./services/auth/Send_Reset_password";
 import ResetPasswordInEmail from "./services/auth/Reset_Password_In_Email";
@@ -35,30 +22,9 @@ import ConfirmOtpCode from "./services/auth/Form_Confirm_OTP";
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <>
-      <div className="sticky top-0 z-50">
-        <Navbar />
-      </div>
-
-      <Hero_section />
-
-      {/* Test Pages */}
-      <Design_checkout_page />
-      <Form_Confirm_OTP />
-
-      {/* Auth Pages */}
-      {/* 
-      <Registerform />
-      <Reset_password />
-      <ResetPassword />
-      */}
-
-      <Footer />
-    </>
-=======
-    // <Router>
+    <Router>
       <Routes>
+
         {/* Public Pages with Navbar/Footer */}
         <Route element={<PublicLayout />}>
           <Route path="/" element={<Home />} />
@@ -77,9 +43,9 @@ const App = () => {
           <Route path="/send_otp_code" element={<SendOtpCode />} />
           <Route path="/confirm_otp_code" element={<ConfirmOtpCode />} />
         </Route>
+
       </Routes>
-    // </Router>
->>>>>>> a8b59b019b668694f02e65b3ca49cc106f249cd4
+    </Router>
   );
 };
 
