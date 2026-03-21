2347ea6e46501ed15d7aa9d36b5891961b836057
NalenSrin123 <srinnalen@gmail.com>
2026-03-21 14:30:03 +0700

    Last version

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,30 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 // Layouts
-// import PublicLayout from "./app/layouts/PublicLayout";
-// import AuthLayout from "./app/layouts/AuthLayout";
+import PublicLayout from "./app/layouts/PublicLayout";
+import AuthLayout from "./app/layouts/AuthLayout";
 
 
 // Public Pages
 import Home from "./components/public/home/Home_page";
-// import MenuPage from "./components/public/menu/Menu_page";
-// import ServicePage from "./components/public/service/Design_Service_Page";
-// import About from "./components/public/about/About";
+import MenuPage from "./components/public/menu/Menu_page";
+import ServicePage from "./components/public/service/Design_Service_Page";
+import About from "./components/public/about/About";
 
 
 // Auth Pages
-// import Login from "./context/auth/form_login";
-// import Register from "./services/auth/Registerform";
-// import ResetPassword from "./services/auth/ResetPassword";
-// import SendResetPassword from "./services/auth/Send_Reset_password";
-// import ResetPasswordInEmail from "./services/auth/Reset_Password_In_Email";
-// import SendOtpCode from "./services/auth/Sendotpcode";
-// import ConfirmOtpCode from "./services/auth/Form_Confirm_OTP";
+import Login from "./context/auth/form_login";
+import Register from "./services/auth/Registerform";
+import ResetPassword from "./services/auth/ResetPassword";
+import SendResetPassword from "./services/auth/Send_Reset_password";
+import ResetPasswordInEmail from "./services/auth/Reset_Password_In_Email";
+import SendOtpCode from "./services/auth/Sendotpcode";
+import ConfirmOtpCode from "./services/auth/Form_Confirm_OTP";
 
 // Dashboard 
 import CoffeeDashboardLayout from "./dashboard/pages/SSidebar_header";
+import Checkout from "./components/public/checkout/Design_cart_page";
+import ContactPage from "./components/public/contact/Contact_Page";
 
 
 
@@ -32,16 +34,18 @@ const App = () => {
     <Routes>
        
        
-        {/* <Route element={<PublicLayout />}>
+        <Route element={<PublicLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<MenuPage />} />
           <Route path="/service" element={<ServicePage />} />
           <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/cart" element={<Checkout />} />
           
-        </Route> */}
+        </Route>
 
         {/* Auth */}
-        {/* <Route element={<AuthLayout />}>
+        <Route element={<AuthLayout />}>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/send-reset-password" element={<SendResetPassword />} />
@@ -49,13 +53,10 @@ const App = () => {
           <Route path="/send-reset-password-in-email" element={<ResetPasswordInEmail />} />
           <Route path="/send_otp_code" element={<SendOtpCode />} />
           <Route path="/confirm_otp_code" element={<ConfirmOtpCode />} />
-        </Route> */}
-
-        {/* Dashboard */}
+          <Route path="/dashboard" element={<CoffeeDashboardLayout />} />
+        </Route>
         
           
-        
-        <Route path="/dashboard" element={<CoffeeDashboardLayout />} />
 
       </Routes>
   );
